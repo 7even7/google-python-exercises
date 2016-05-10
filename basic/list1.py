7b76ee1649b417a643989a0c2ebb4fb0f18d821c
7even7 <patrick.stahl@gmail.com>
2016-05-10 11:03:52 +0300

    List1 ekatehtävä

--- basic/list1.py
+++ basic/list1.py
@@ -21,8 +21,11 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-  # +++your code here+++
-  return
+  lkm = 0
+  for word in words:
+    if len(word)>=2  and word[0] == word[-1]:
+      lkm +=1
+  return lkm
 
 
 # B. front_x
